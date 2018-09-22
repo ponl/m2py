d91ae822605500cda040e31fbe9b6b62fdeb5f65
Diego Torrejon <diego.torrejon.medina@gmail.com>
2018-09-22 13:12:59 -0400

    Add correlation analysis

--- methods/main_methods.py
+++ methods/main_methods.py
@@ -4,9 +4,11 @@ from scipy import signal
 from matplotlib import pyplot
 
 from methods import config
+import seaborn as sb
 
-fields = config.data_properties
+PROPS = config.data_properties
 
+## Boundary Detection
 def sobel(x):
     """ Applies the Sobel operator to x
     Args:
@@ -28,10 +30,10 @@ def show_boundaries(X):
     """
     fig = pyplot.figure(figsize=(16, 30), dpi= 80, facecolor='w', edgecolor='k')
     cnt = 1
-    for i in range(6):
+    for i in range(X.shape[2]):
         x = X[:,:,i]
-        pyplot.subplot(3,4,cnt)
-        pyplot.title(fields[i])
+        pyplot.subplot(3,4,cnt) # TODO how to ensure break down of plots?
+        pyplot.title(PROPS[i])
         m = pyplot.imshow(x)
         pyplot.colorbar(m, fraction=0.046, pad=0.04)
 
@@ -39,7 +41,7 @@ def show_boundaries(X):
 
         # Apply Sobel operator
         pyplot.subplot(3,4,cnt)
-        pyplot.title(f"{fields[i]} Gradient")
+        pyplot.title(f"{PROPS[i]} Gradient")
         m = pyplot.imshow(sobel(x))
         pyplot.colorbar(m, fraction=0.046, pad=0.04)
 
@@ -48,26 +50,31 @@ def show_boundaries(X):
     pyplot.tight_layout()
     pyplot.show()
 
-def show_feature_distributions(X):
+## Properties Distributions
+def show_property_distributions(X):
     """ Plots the pdfs of the data properties
     Args:
         X (np array): data
     """
     O = extract_outliers(X)
-    pyplot.figure(figsize=(15, 10), dpi= 80, facecolor='w', edgecolor='k')
+    fig = pyplot.figure(figsize=(15, 10), dpi= 80, facecolor='w', edgecolor='k')
 
-    for j in range(6):
+    for j in range(X.shape[2]):
         x = [X[n,m,j] for n in range(X.shape[0]) for m in range(X.shape[1]) if not O[n,m]]
-        pyplot.subplot(3, 3, j+1)
-        pyplot.title(fields[j])
-        pyplot.hist(x, 50, density=True)
+        pyplot.subplot(2, 3, j+1) # TODO how to ensure break down of plots?
+        pyplot.title(PROPS[j])
+        sb.distplot(x) # TODO warning gets thrown out when using this
+        pyplot.grid()
 
     pyplot.show()
 
-def extract_outliers(X, height_index = 4, threshold=2.5):
+## Outlier Detection
+def extract_outliers(X, height_index=4, threshold=2.5):
     """ Finds outliers from data
     Args:
         X (np array): data
+        height_index (int): index of height property
+        threshold: (float): z-score threshold
     Returns:
         (np array): boolean matrix denoting outliers
     """
@@ -85,15 +92,16 @@ def extract_outliers(X, height_index = 4, threshold=2.5):
     # Threshold by z-score
     return z > threshold
 
-def show_outliers(X):
+def show_outliers(X, height_index=4):
     """ Plots data properties and outliers
     Args:
         X (np array): data
+        height_index (int): index of height property
     """
     fig = pyplot.figure(figsize=(15,5))
 
     pyplot.subplot(1,3,1)
-    m = pyplot.imshow(X[:,:,4])
+    m = pyplot.imshow(X[:,:,height_index])
     pyplot.title('Height')
     pyplot.colorbar(m, fraction=0.046, pad=0.04)
 
@@ -104,8 +112,121 @@ def show_outliers(X):
     outliers = extract_outliers(X)
     X[outliers == 1] = 0
     pyplot.subplot(1,3,3)
-    m = pyplot.imshow(X[:,:,4])
+    m = pyplot.imshow(X[:,:,height_index])
     pyplot.title('Height')
     pyplot.colorbar(m, fraction=0.046, pad=0.04)
     pyplot.show()
+
+## Features Correlations
+def get_all_paths(path):
+    """ Gets list of data files in path
+    Args:
+        path (str): data directory
+    Returns:
+        fls (list): data files
+    """
+    fls = [os.path.join(path, f) for f in os.listdir(path)]
+    return fls
+
+def get_sample_count(path):
+    """ Gets number of data files in path
+    Args:
+        path (str): data directory
+    Returns:
+        (int): number of data files
+    """
+    return len(get_all_paths(path))
+
+def get_path_from_index(index, path):
+    """ Gets specific data file from path
+    Args:
+        index (int): specific data file index
+        path (str): data directory
+    Returns:
+        (str): data file
+    """
+    return get_all_paths(path)[index]
+
+def get_data(index, path):
+    """ Loads specific data file
+    Args:
+        index (int): specific data file index
+        path (str): data directory
+    Returns:
+       (np.array): data from file
+    """
+    fl = get_path_from_index(index, path)
+    return np.load(fl)
+
+def get_correlations(path):
+    """ Computes correlation for all files in path
+    Args:
+        path (str): data directory
+    Returns:
+        cors (list): correlation between all pairs of properties per file
+    """
+    N = get_sample_count(path)
+    cors = []
+    for i in range(N):
+        X = get_data(i, path)
+        d = X.shape[2]
+
+        C = np.zeros((d,d)) # correlation matrix of properties per file
+        for j in range(d):
+            for k in range(j, d):
+                C[j,k] = C[k,j] = np.corrcoef(X[:,:,j].flatten(), X[:,:,k].flatten())[0][1]
+
+        cors.append(C)
+
+    return cors
+
+def get_correlation_values(cors, r, c):
+    """ Gets correlation between properties r and c for all files
+    Args:
+        cors (list): propertiess correlations per file
+        r (int): first property index
+        c (int): second property index
+    Returns:
+        rc_cors (list): correlation between properties r and c per file
+    """
+    rc_cors = [cor[r,c] for cor in cors]
+    return rc_cors
+
+def plot_correlations(num_props, path):
+    """ Plots correlations between all properties
+    Args:
+        num_props (int): number of properties
+        path (str): data directory
+    """
+    cors = get_correlations(path)
+
+    fig = pyplot.figure(figsize=(20, 30))
+    fig.subplots_adjust(hspace=1)
+
+    cnt = 1
+    for i in range(num_props):
+        for j in range(num_props):
+            pyplot.subplot(num_props, num_props, cnt)
+            pyplot.title(f"{PROPS[i]} -- {PROPS[j]}")
+
+            if i == j: # skip auto-correlation
+                pyplot.xlabel("Correlation")
+                pyplot.xlim(-1,1)
+                pyplot.grid()
+                cnt += 1
+                continue
+
+            P = get_correlation_values(cors, i, j)
+            V = [np.round(p, 1) for p in P if not np.isnan(p)]
+
+            sb.distplot(V) # TODO warning gets thrown out when using this
+            pyplot.xlabel("Correlation")
+            pyplot.xlim(-1,1)
+            pyplot.grid()
+
+            cnt += 1
+
+    pyplot.show()
+
+## Mixture of Gaussians Model
 
--- methods/main_script.py
+++ methods/main_script.py
@@ -4,16 +4,19 @@ from methods import main_methods as mm
 import numpy as np
 
 DATA_DIR = config.data_directory_path
+DATA_TYPE_SUB_DIR = "2componentfilms/combined/"
+DIR = DATA_DIR + DATA_TYPE_SUB_DIR
 
 # Using wire films
 #X = np.load(DATA_DIR + "nanowires/combined/100-0_48_NW_combined.npy")
 
 # Using 2 component nano films
-X = np.load(DATA_DIR + "2componentfilms/combined/PTB7-ThPC71BM_CBDIO_vacm_fresh_3um_combined.npy")
+X = np.load(DIR + "PTB7-ThPC71BM_CBDIO_vacm_fresh_3um_combined.npy")
 
-mm.show_boundaries(X)
-mm.show_outliers(X)
+#mm.show_boundaries(X)
+#mm.show_outliers(X)
 
-mm.show_feature_distributions(X)
+#mm.show_property_distributions(X)
+#mm.plot_correlations(X.shape[2], DIR) # TODO needs to be tested with more data files
 
 original_x = X.copy() # TODO
