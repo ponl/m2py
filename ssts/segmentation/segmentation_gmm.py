6bab2c838b933ceafc217d3b9fdc57b7dc3bc4ae
Patrick O'Neil <paddyoneil89@gmail.com>
2018-09-30 14:09:04 -0400

    Fixing issues with GMM Segmenter`

--- ssts/segmentation/segmentation_gmm.py
+++ ssts/segmentation/segmentation_gmm.py
@@ -8,22 +8,34 @@ from sklearn.decomposition import PCA
 logger = logging.getLogger(__name__)
 
 class SegmenterGMM(object):
-    def __init__(self, n_components=3, normalize=True, embedding_dim=None, padding=0):
+    def __init__(self, n_components=3, normalize=True, embedding_dim=None, padding=0, zscale=False):
         """
+        Perform material segmentation using a Gaussian Mixture Model. Various ways of building features
+        and performing dimensionality reduction are provided. If `padding` is set greater than zero, local
+        neighborhoods around each pixel will be used. These neighborhoods will be flattened into vectors
+        and fed through the dimensionality reduction step. If `padding` is zero, no neighborhood information
+        will be used. The `embedding_dim` value will determine whether to use dimensionality reduction and,
+        if so, what the embedded dimension should be. The dimensionality step will be performed prior to
+        fitting the Gaussian mixture model.
 
         Args:
             n_components (int): Number of components (i.e. segments) to learn in the GMM.
             normalize (bool): Normalize the material properties before processing.
+            embedding_dim (int): Dimension used in dimensionality reduction.
+            padding (int): Padding used in sliding window. When set to zero, no neighbor information
+                will be used. The neighbor values are flattened and fed into the PCA routine.
+            zscale (bool): Whether to use z-score scaling (True) or scale by property maxima (False).
         """
-
         self.normalize = normalize
         self.embedding_dim = embedding_dim
         self.n_components = n_components
         self.padding = padding
+        self.zscale = zscale
 
         self.gmm = None
         self.pca = None
         self.sst = None
+
 
     def fit(self, data, outliers=None):
         """
@@ -41,7 +53,7 @@ class SegmenterGMM(object):
             logger.warning("Data arrays must be of shape (height, width, n_properties).")
             return None
 
-        if outliers and len(outliers.shape) != 2:
+        if outliers is not None and len(outliers.shape) != 2:
             logger.warning("Outlier arrays must be of shape (height, width).")
             return None
 
@@ -49,28 +61,26 @@ class SegmenterGMM(object):
         n = h * w
 
         if self.padding > 0:
-            print("Building Windows")
             data = SegmenterGMM.get_windows(data, padding=self.padding)
         else:
             data = data.reshape(n, c)
 
         if outliers is not None:
-            print("Removing Outliers")
-            outliers = outliers.reshape(n, c)
+            outliers = outliers.flatten()
 
         data = SegmenterGMM.remove_outliers(data, outliers)
 
         if self.normalize:
-            print("Scaling data")
-            self.sst = StandardScaler()
-            data = self.sst.fit_transform(data)
+            if self.zscale:
+                self.sst = StandardScaler()
+                data = self.sst.fit_transform(data)
+            else:
+                data = SegmenterGMM.normalize_by_max(data)
 
         if self.embedding_dim is not None:
-            print("Fitting PCA")
             self.pca = PCA(n_components=self.embedding_dim)
             data = self.pca.fit_transform(data)
 
-        print("Fitting GMM")
         self.gmm = GaussianMixture(n_components=self.n_components, covariance_type="full")
         self.gmm.fit(data)
 
@@ -98,29 +108,25 @@ class SegmenterGMM(object):
         n = h * w
 
         if self.padding > 0:
-            print("Building Windows")
             data = SegmenterGMM.get_windows(data, padding=self.padding)
         else:
             data = data.reshape(n, c)
 
         if outliers is not None:
-            print("Removing Outliers")
-            outliers = outliers.reshape(n, c)
+            outliers = outliers.flatten()
 
         data = SegmenterGMM.remove_outliers(data, outliers)
 
         if self.normalize:
-            print("Scaling Data")
-            data = self.sst.transform(data)
+            data = self.sst.transform(data) if self.zscale else SegmenterGMM.normalize_by_max(data)
 
         if self.embedding_dim is not None:
-            print("PCA Transform")
             data = self.pca.transform(data)
 
-        print("Predicting with GMM")
         labels = self.gmm.predict(data)
 
         return np.reshape(labels, (h, w))
+
 
     def fit_transform(self, data, outliers=None):
         """
@@ -149,7 +155,7 @@ class SegmenterGMM(object):
             return data
 
         n_outlier = len(outliers)
-        return np.array([data[i] for i in range(n_outliers) if not outliers[i]])
+        return np.array([data[i] for i in range(n_outlier) if not outliers[i]])
 
 
     @staticmethod
@@ -173,3 +179,7 @@ class SegmenterGMM(object):
 
         return wins
 
+    @staticmethod
+    def normalize_by_max(data):
+        m = np.max(np.abs(data), axis=0)
+        return data / m
